f82afb9af60372fbbbd1a6bc3e87092ee801de3e
RealityStop <qubiquityCodes@gmail.com>
2018-02-07 01:39:44 -0500

    Attempt at Fody.

---
+++ src/CosturaLoader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bolt.Addons.Community
+{
+    public static class CosturaLoader
+    {
+        static CosturaLoader()
+        {
+            CosturaUtility.Initialize();
+        }
+    }
+}
