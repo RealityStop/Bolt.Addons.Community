e599f75871f2cabf6cbd1bd896f7c196ae5e9b0a
Jason Jones <info@lifeandstylemedia.com>
2018-05-31 09:37:10 -0400

    Added Return Events

---
+++ src/Events/Editor/Bolt.Addons.Community.Events.Editor/TriggerReturnEventDescriptor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ludiq;
+using Bolt;
+
+namespace Bolt.Addons.Community.Events {
+    [Widget(typeof(TriggerReturnEvent))]
+    public class TriggerReturnEventWidget : UnitWidget<TriggerReturnEvent>
+    {
+        public TriggerReturnEventWidget(TriggerReturnEvent unit) : base(unit)
+        {
+
+        }
+
+        protected override NodeColorMix baseColor
+        {
+            get
+            {
+                return NodeColor.Gray;
+            }
+        }
+
+    }
+
+    public class TriggerReturnEventDescriptor : UnitWidget<TriggerReturnEvent>
+    {
+        public TriggerReturnEventDescriptor(TriggerReturnEvent unit) : base(unit)
+        {
+
+        }
+        
+
+    }
+}
