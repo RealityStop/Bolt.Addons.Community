1e6eb395a512562bcbbd042567cf65b4f3ab8881
Jason Jones <info@lifeandstylemedia.com>
2018-06-01 02:19:19 -0400

    Added back in the Descriptor to Trigger Return Event.

---
+++ src/Events/Editor/TriggerReturnEventDescriptor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ludiq;
+using Bolt;
+
+namespace Bolt.Addons.Community.Events {
+    [Descriptor(typeof(TriggerReturnEvent))]
+    public class TriggerReturnEventDescriptor : UnitDescriptor<TriggerReturnEvent>
+    {
+        public TriggerReturnEventDescriptor(TriggerReturnEvent unit) : base(unit)
+        {
+
+        }
+    }
+}
