eca37de45d3ecf26b885b573d35408196004479b
RealityStop <qubiquityCodes@gmail.com>
2019-01-23 00:23:44 -0500

    Added SomeValue and reverted Events inspector

---
+++ src/Events/Inspectors/DefinedEventInspector.cs
@@ -0,0 +1,40 @@
+﻿using Bolt.Addons.Community.DefinedEvents.Units;
+using Ludiq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+
+
+namespace Ludiq
+{
+    //[Inspector(typeof(TargettedDefinedEvent))]
+    //public class DefinedEventInspector : ReflectedInspector
+    //{
+    //    public DefinedEventInspector(Metadata metadata) : base(metadata)
+    //    {
+    //        Debug.Log($"hi");
+    //    }
+
+    //    protected override bool Include(MemberInfo m)
+    //    {
+
+    //        if (m.Name == "eventType")
+    //        {
+    //            return true;
+    //            //return (bool)metadata["IsNotRestricted"].value;
+    //        }
+
+
+    //        if (m.Name == "restrictedEventType")
+    //        {
+    //            return true;
+    //            //return (bool)metadata["IsRestricted"].value;
+    //        }
+
+    //        return base.Include(m);
+    //    }
+    //}
+}
---
+++ src/Events/Widgets/DefinedEventWidget.cs
@@ -0,0 +1,56 @@
+﻿using Bolt.Addons.Community.DefinedEvents.Units;
+using Ludiq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace Bolt.Addons.Community.DefinedEvents.Editor.Widgets
+{
+    //[Widget(typeof(TargettedDefinedEvent))]
+    //public sealed class DefinedEventWidget : UnitWidget<TargettedDefinedEvent>
+    //{
+    //    public DefinedEventWidget(FlowCanvas canvas, TargettedDefinedEvent unit) : base(canvas, unit)
+    //    {
+    //        Debug.Log($"hi2");
+    //    }
+
+    //    protected override float GetHeaderAddonHeight(float width)
+    //    {
+    //        return EditorGUIUtility.singleLineHeight;
+    //    }
+
+    //    public override void BeforeFrame()
+    //    {
+    //        base.BeforeFrame();
+
+    //        if (GetHeaderAddonWidth() != headerAddonPosition.width ||
+    //            GetHeaderAddonHeight(headerAddonPosition.width) != headerAddonPosition.height)
+    //        {
+    //            Reposition();
+    //        }
+    //    }
+
+    //    protected override void DrawHeaderAddon()
+    //    {
+    //        using (LudiqGUIUtility.labelWidth.Override(75)) // For reflected inspectors / custom property drawers
+    //        using (Inspector.adaptiveWidth.Override(true))
+    //        {
+    //            EditorGUI.BeginChangeCheck();
+
+    //            if (unit.IsRestricted)
+    //                LudiqGUI.Inspector(metadata["restrictedEventType"], headerAddonPosition, GUIContent.none);
+    //            else
+    //                LudiqGUI.Inspector(metadata["eventType"], headerAddonPosition, GUIContent.none);
+
+    //            if (EditorGUI.EndChangeCheck())
+    //            {
+    //                unit.EnsureDefined();
+    //                Reposition();
+    //            }
+    //        }
+    //    }
+    //}
+}
