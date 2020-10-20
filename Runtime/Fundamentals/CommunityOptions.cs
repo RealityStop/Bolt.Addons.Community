d9e68f50f15ebd941329aad91a4104e30245c5c0
RealityStop <qubiquityCodes@gmail.com>
2020-10-20 02:56:19 -0400

    Added preserve option to the Fundamentals asmdef

--- Runtime/Fundamentals/CommunityOptions.cs
+++ Runtime/Fundamentals/CommunityOptions.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine.Scripting;
+
+
+[assembly:Preserve]
 
 namespace Bolt.Addons.Community
 {
