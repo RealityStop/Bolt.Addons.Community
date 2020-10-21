9367585d7b5e858245a80f8fccb2233e623366dd
Jason Jones <jasonjones3d@gmail.com>
2020-10-20 23:57:52 -0400

    Merged ObjectDictionary and ObjectList from UAlive/Bolt Extensions

---
+++ Runtime/Utility/ObjectDictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Ludiq;
+
+namespace Bolt.Addons.Community.Utility
+{
+    /// <summary>
+    /// An AOTDictuonary replacement that can be serialized and saved.
+    /// </summary>
+    [Serializable][Inspectable]
+    [IncludeInSettings(true)]
+    [RenamedFrom("Lasm.BoltExtensions.IO.ObjectDictionary")]
+    [RenamedFrom("Lasm.BoltExtensions.ObjectDictionary")]
+    [RenamedFrom("Lasm.UAlive.ObjectDictionary")]
+    public sealed class ObjectDictionary : Dictionary<object, object> { }
+}
+
---
+++ Runtime/Utility/ObjectList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Ludiq;
+
+namespace Bolt.Addons.Community.Utility
+{
+    /// <summary>
+    /// An AOTDictuonary replacement that can be serialized and saved.
+    /// </summary>
+    [Serializable][Inspectable][IncludeInSettings(true)]
+    [RenamedFrom("Lasm.BoltExtensions.IO.ObjectList")]
+    [RenamedFrom("Lasm.BoltExtensions.ObjectList")]
+    [RenamedFrom("Lasm.UAlive.ObjectList")]
+    public sealed class ObjectList : List<object> { }
+}
+
