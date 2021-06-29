6bd6dcb398d85b5cb507b886d566a654c3a0600c
Jason Jones <jasonjones3d@gmail.com>
2021-06-29 02:20:47 -0400

    Added Coroutine and Yield Instruction to Wait For Enumerator

--- Runtime/Fundamentals/Units/Logic/Control/WaitForEnumerator.cs
+++ Runtime/Fundamentals/Units/Logic/Control/WaitForEnumerator.cs
@@ -8,20 +8,59 @@ namespace Bolt.Addons.Community.Fundamentals
     [UnitCategory("Community/Time")]
     public class WaitForEnumerator : WaitUnit
     {
+        [UnitHeaderInspectable]
+        public EnumeratorType type;
+
         [DoNotSerialize]
-        public ValueInput enumerator;
+        public ValueInput enumerator, instruction, coroutine;
 
         protected override void Definition()
         {
             base.Definition();
 
-            enumerator = ValueInput<IEnumerator>("enumerator");
+            switch (type)
+            {
+                case EnumeratorType.YieldInstruction:
+                    instruction = ValueInput<YieldInstruction>("instruction");
+                    break;
+
+                case EnumeratorType.Enumerator:
+                    enumerator = ValueInput<IEnumerator>("enumerator");
+                    break;
+
+                case EnumeratorType.Coroutine:
+                    coroutine = ValueInput<Coroutine>("coroutine");
+                    break;
+            }
         }
         protected override IEnumerator Await(Flow flow)
         {
-            var _enumerator = flow.GetValue<IEnumerator>(enumerator);
-            while (_enumerator.MoveNext()) yield return _enumerator.Current;
+            switch (type)
+            {
+                case EnumeratorType.YieldInstruction:
+                    var _instruction = flow.GetValue<YieldInstruction>(instruction);
+                    yield return _instruction;
+                    break;
+
+                case EnumeratorType.Enumerator:
+                    var _enumerator = flow.GetValue<IEnumerator>(enumerator);
+                    while (_enumerator.MoveNext()) yield return _enumerator.Current;
+                    break;
+
+                case EnumeratorType.Coroutine:
+                    var _coroutine = flow.GetValue<Coroutine>(coroutine);
+                    yield return _coroutine;
+                    break;
+            }
+
             yield return exit;
+        }
+
+        public enum EnumeratorType
+        {
+            YieldInstruction,
+            Enumerator,
+            Coroutine
         }
     }
 }
