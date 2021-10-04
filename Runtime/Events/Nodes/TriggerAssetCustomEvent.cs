3da2a809c336d29ba00a3253ab623c3a07c0ba75
Jason Jones <jasonjones3d@gmail.com>
2021-10-04 14:15:51 -0400

    Added Trigger Asset Custom Event
    Triggers a custom event directly on an asset. Beware, using an asset as the reference to trigger, when modifying variables, will be permanent.

---
+++ Runtime/Events/Nodes/TriggerAssetCustomEvent.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unity.VisualScripting.Community
+{
+    [TypeIcon(typeof(EventUnit<CustomEventArgs>))]
+    [UnitCategory("Events/Community")]
+    [RenamedFrom("Lasm.AssetLibrary.Nodes.TriggerAssetCustomEvent")]
+    public sealed class TriggerAssetCustomEvent : Unit
+    {
+        [Serialize]
+        private int _count;
+        [UnitHeaderInspectable]
+        [Inspectable]
+        [InspectorLabel("Count")]
+        public int count
+        {
+            get => _count;
+
+            set
+            {
+                _count = Mathf.Clamp(value, 0, 10);
+            }
+        }
+
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ControlInput enter;
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ControlOutput exit;
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ValueInput asset;
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ValueInput name;
+        [DoNotSerialize]
+        public List<ValueInput> args = new List<ValueInput>();
+
+        protected override void Definition()
+        {
+            enter = ControlInput("enter", (flow) =>
+            {
+                var argList = new List<object>();
+
+                for (int i = 0; i < args.Count; i++)
+                {
+                    argList.Add(flow.GetValue<object>(args[i]));
+                }
+
+                GraphReference.New(flow.GetValue<ScriptGraphAsset>(asset), true).TriggerEventHandler<CustomEventArgs>(hook => hook == "Custom", new CustomEventArgs(flow.GetValue<string>(name), argList.ToArray()), parent => true, true);
+
+                return exit;
+            });
+
+            asset = ValueInput<ScriptGraphAsset>("asset", (ScriptGraphAsset)null);
+            name = ValueInput<string>("name", string.Empty);
+
+            args.Clear();
+
+            for (int i = 0; i < count; i++)
+            {
+                args.Add(ValueInput<object>($"arg {i.ToString()}"));
+                Requirement(args[i], enter);
+            }
+
+            exit = ControlOutput("exit");
+
+            Requirement(asset, enter);
+            Succession(enter, exit);
+        }
+    }
+}
