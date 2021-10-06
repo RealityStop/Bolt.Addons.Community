e92f97a872ce6fb90d011e74e8f891935f0d851b
Jason Jones <jasonjones3d@gmail.com>
2021-10-06 15:57:43 -0400

    Added Reset Graph Listener

---
+++ Runtime/Events/Nodes/ResetGraphListener.cs
@@ -0,0 +1,28 @@
+﻿namespace Unity.VisualScripting.Community
+{
+    [TypeIcon(typeof(EventUnit<CustomEventArgs>))]
+    [UnitCategory("Events/Community")]
+    public sealed class ResetGraphListener : Unit
+    {
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ControlInput enter;
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ControlOutput exit;
+
+        protected override void Definition()
+        {
+            enter = ControlInput("enter", (flow) =>
+            {
+                graph.StopListening(flow.stack.AsReference());
+                graph.StartListening(flow.stack.AsReference());
+                return exit;
+            });
+
+            exit = ControlOutput("exit");
+
+            Succession(enter, exit);
+        }
+    }
+}
