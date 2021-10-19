3b1ec116694ed20dbcf99d5d5d5f64ba0edfc88c
Jason Jones <jasonjones3d@gmail.com>
2021-10-19 14:28:11 -0400

    Added Enumerator Node to output a Control Routine as an IEnumerator

---
+++ Runtime/Fundamentals/Nodes/Logic/Control/EnumeratorNode.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+
+namespace Unity.VisualScripting.Community
+{
+    [UnitTitle("Enumerator")]
+    [UnitCategory("Community/Time")]
+    [TypeIcon(typeof(WaitUnit))]
+    public sealed class EnumeratorNode : Unit
+    {
+        public override bool isControlRoot => true;
+
+        [PortLabelHidden]
+        public ControlOutput trigger;
+        [PortLabelHidden]
+        public ValueOutput enumerator;
+
+        protected override void Definition()
+        {
+            trigger = ControlOutput("trigger");
+            enumerator = ValueOutput("enumerator", Run);
+        }
+
+        public IEnumerator Run(Flow flow)
+        {
+            yield return trigger;
+        }
+    }
+}
