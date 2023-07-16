be34485a32a2d0488dfb8c26961fd5f0e49344ba
S2NX7 <125309059+S2NX7@users.noreply.github.com>
2023-07-16 15:11:44 +0200

    Add files via upload

---
+++ Runtime/Fundamentals/Nodes/Object Pooling/Nodes/ReturnAllObjects.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.VisualScripting;
+
+    [UnitCategory("Object Pooling")]
+    [UnitTitle("Return All")]
+    public class ReturnAllObjectsToPoolNode : Unit
+    {
+        [DoNotSerialize]
+        public ControlInput Enter;
+
+        [DoNotSerialize]
+        public ControlOutput Exit;
+
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ValueInput Pool;
+
+        protected override void Definition()
+        {
+            Enter = ControlInput(nameof(Enter), OnEnter);
+            Exit = ControlOutput(nameof(Exit));
+
+            Pool = ValueInput<CustomObjectPool>(nameof(Pool));
+
+            Succession(Enter, Exit);
+        }
+
+        private ControlOutput OnEnter(Flow flow)
+        {
+            var pool = flow.GetValue<CustomObjectPool>(Pool);
+
+            if (pool != null)
+            {
+                List<GameObject> activeObjectsCopy = new List<GameObject>(pool.GetActiveObjects());
+
+                foreach (var obj in activeObjectsCopy)
+                {
+                    pool.ReturnObjectToPool(obj);
+                }
+            }
+
+            return Exit;
+        }
+    }
+
