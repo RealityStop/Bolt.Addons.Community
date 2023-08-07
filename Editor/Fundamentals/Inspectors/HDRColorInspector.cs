16e4426edd5ae37f4078a0228d3aeba7a7ae26a6
S2NX7 <125309059+S2NX7@users.noreply.github.com>
2023-08-07 20:49:16 +0200

    Added Inspectors

---
+++ Editor/Fundamentals/Inspectors/HDRColorInspector.cs
@@ -0,0 +1,34 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Unity.VisualScripting
+{
+    [Inspector(typeof(HDRColor))] // Update this to use HDRColor instead of Color
+    public class HDRColorInspector : Inspector
+    {
+        public HDRColorInspector(Metadata metadata) : base(metadata) { }
+
+        protected override float GetHeight(float width, GUIContent label)
+        {
+            return EditorGUIUtility.singleLineHeight;
+        }
+
+        protected override void OnGUI(Rect position, GUIContent label)
+        {
+            position = BeginLabeledBlock(metadata, position, label);
+
+            // Retrieve the color value from the HDRColor struct
+            HDRColor hdrColor = (HDRColor)metadata.value;
+            var newValue = EditorGUI.ColorField(position, new(), hdrColor.color, true, true, true);
+
+            if (EndBlock(metadata))
+            {
+                metadata.RecordUndo();
+
+                // Update the color value in the HDRColor struct
+                hdrColor.color = newValue;
+                metadata.value = hdrColor;
+            }
+        }
+    }
+}
