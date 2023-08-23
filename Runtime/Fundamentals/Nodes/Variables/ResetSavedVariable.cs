1420abb8bb5d59782bfa2fb5070965293e16f3a9
S2NX7 <tauqeer20041@gmail.com>
2023-08-23 23:27:23 +0200

    Added Reset Saved Variables

---
+++ Runtime/Fundamentals/Nodes/Variables/ResetSavedVariable.cs
@@ -0,0 +1,67 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[UnitCategory("Community/Variables")]
+[UnitTitle("ResetSavedVariable")]//Unit title
+[TypeIcon(typeof(FlowGraph))]//Unit icon
+public class ResetSavedVariable : Unit
+{
+    [SerializeAs(nameof(_argumentCount))]
+    private int _argumentCount;
+
+    [UnitHeaderInspectable("Keys")]
+    [DoNotSerialize]
+    public int KeyCount {
+        get
+        {
+            return Mathf.Max(1, _argumentCount);
+        }
+        set
+        {
+            _argumentCount = Mathf.Clamp(value, 1, 10);
+        }
+    }
+    
+    [DoNotSerialize]
+    [PortLabelHidden]
+    public ControlInput Reset;
+    [DoNotSerialize]
+    [PortLabelHidden]
+    public ControlOutput OnReset;
+    [DoNotSerialize]
+    public List<ValueInput> Keys;
+
+    protected override void Definition()
+    {
+        Keys = new List<ValueInput>();
+
+        Reset = ControlInput(nameof(Reset), _Enter_);
+        OnReset = ControlOutput(nameof(OnReset));
+
+        for (int i = 0; i < KeyCount; i++)
+        {
+            var input = ValueInput<string>("Key " + i, "");
+            Requirement(input, Reset);
+            Keys.Add(input);
+        }
+
+        Succession(Reset, OnReset);
+    }
+
+    public ControlOutput _Enter_(Flow flow) 
+    {
+        foreach (var Key in Keys)
+        {
+            string key = flow.GetValue<string>(Key);
+
+            var initalvariable = SavedVariables.initial[key];
+            SavedVariables.saved[key] = initalvariable;
+
+            SavedVariables.current[key] = initalvariable;
+
+            SavedVariables.SaveDeclarations(SavedVariables.saved);
+        }
+        return OnReset;
+    }
+}
