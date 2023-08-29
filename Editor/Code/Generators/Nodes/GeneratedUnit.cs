4e746fcb334fe7244e57cf0534ed548752e6ebb5
S2NX7 <tauqeer20041@gmail.com>
2023-08-29 21:38:47 +0200

    AddedReference

--- Editor/Code/Generators/Nodes/GeneratedUnit.cs
+++ Editor/Code/Generators/Nodes/GeneratedUnit.cs
@@ -2,42 +2,43 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
-using Unity.VisualScripting;
-using Unity.VisualScripting.Community;
 using UnityEngine;
 
-[UnitCategory("Community/CodeGenerators")]
-public abstract class GeneratedUnit : Unit
+namespace Unity.VisualScripting.Community
 {
-    [DoNotSerialize]
-    [PortLabelHidden]
-    public ControlOutput Exit;
-
-    [DoNotSerialize]
-    [PortLabelHidden]
-    public ControlInput Enter;
-
-    protected override void Definition()
+    [UnitCategory("Community/CodeGenerators")]
+    public abstract class GeneratedUnit : Unit
     {
-        Enter = ControlInput(nameof(Enter), Logic);
-        Exit = ControlOutput(nameof(Exit));
-        Succession(Enter, Exit);
-    }
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ControlOutput Exit;
 
-    public virtual ControlOutput Logic(Flow flow) 
-    {
-        Debug.LogWarning("This node is only for the code generators to understand what to do it does not work in a normal graph");
-        return Exit;
-    }
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ControlInput Enter;
 
-    public virtual string GeneratorLogic(ControlGenerationData data, int indent, NodeGenerator generator) 
-    {
-        return $"/* Create logic Generator */";
-    }
+        protected override void Definition()
+        {
+            Enter = ControlInput(nameof(Enter), Logic);
+            Exit = ControlOutput(nameof(Exit));
+            Succession(Enter, Exit);
+        }
 
-    public virtual string GeneratorOutput(NodeGenerator generator)
-    {
-        return $"/* Create output Generator */";
+        public virtual ControlOutput Logic(Flow flow)
+        {
+            Debug.LogWarning("This node is only for the code generators to understand what to do it does not work in a normal graph");
+            return Exit;
+        }
+
+        public virtual string GeneratorLogic(ControlGenerationData data, int indent, NodeGenerator generator)
+        {
+            return $"/* Create logic Generator */";
+        }
+
+        public virtual string GeneratorOutput(NodeGenerator generator)
+        {
+            return $"/* Create output Generator */";
+        }
     }
 }
 
