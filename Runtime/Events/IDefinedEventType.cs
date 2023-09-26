103e163e5bf67c01c3c7f357ff862fe6b700e1dc
S2NX7 <125309059+S2NX7@users.noreply.github.com>
2023-09-26 11:56:14 +0200

    Add files via upload

---
+++ Runtime/Events/IDefinedEventType.cs
@@ -0,0 +1,11 @@
+using System;
+using Unity.VisualScripting;
+using Unity.VisualScripting.Community;
+
+[System.Serializable]
+[Inspectable]
+public class IDefinedEventType
+{
+    [Inspectable]
+    public Type type = typeof(IDefinedEventType);
+}
